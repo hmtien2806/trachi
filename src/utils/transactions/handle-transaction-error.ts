ffb4a3f943ba114b0664c7bfe3bfbf4bdd44f674
Diep Dang <kdt310722@gmail.com>
2024-04-08 20:38:17 +0700

    feat: handle transaction error

--- src/utils/transactions/handle-transaction-error.ts
+++ src/utils/transactions/handle-transaction-error.ts
@@ -1,4 +1,22 @@
-// eslint-disable-next-line ts/no-unused-vars,unused-imports/no-unused-vars
-export function handleTransactionError(txErr: unknown) {
+import { TransactionExpiredBlockheightExceededError, TransactionExpiredNonceInvalidError, TransactionExpiredTimeoutError } from '@solana/web3.js'
+import { TransactionConfirmFailed } from '../../errors/transaction-confirm-failed'
+
+export function handleTransactionError(error: unknown) {
+    if (error instanceof TransactionExpiredTimeoutError) {
+        return 'Transaction expired: timeout'
+    }
+
+    if (error instanceof TransactionExpiredBlockheightExceededError) {
+        return 'Transaction expired: blockheight exceeded'
+    }
+
+    if (error instanceof TransactionExpiredNonceInvalidError) {
+        return 'Transaction expired: the nonce is no longer valid'
+    }
+
+    if (error instanceof TransactionConfirmFailed) {
+        return `Transaction failed: ${JSON.stringify(error.data)}`
+    }
+
     return 'Transaction failed'
 }
