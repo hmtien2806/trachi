2f7a2d5d0f25b2a62298dbbb03475a22226f0d98
Diep Dang <kdt310722@gmail.com>
2024-04-10 07:02:40 +0700

    refactor: allow empty tokens array

--- src/rpc-methods/tokens/get-tokens.ts
+++ src/rpc-methods/tokens/get-tokens.ts
@@ -3,9 +3,15 @@ import { toJson } from '../../utils/json'
 import { publicKey } from '../../utils/rules/public-key'
 
 export function createGetTokensHandler(): RpcMethod {
-    const schema = publicKey.array().min(1).max(50)
+    const schema = publicKey.array().min(0).max(50)
 
-    return async (params, { token }) => (
-        token.findMultiple(schema.parse(params)).then((i) => toJson(i))
-    )
+    return async (params, { token }) => {
+        const tokens = schema.parse(params)
+
+        if (tokens.length === 0) {
+            return []
+        }
+
+        return token.findMultiple(tokens).then((i) => toJson(i))
+    }
 }
