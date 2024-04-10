a73dcb803bc7a01411cede330d1fa4cca79932a1
Diep Dang <kdt310722@gmail.com>
2024-04-10 07:01:29 +0700

    refactor: increase maximum requests per second

--- src/config/server.ts
+++ src/config/server.ts
@@ -4,7 +4,7 @@ const schema = z.object({
     host: z.string().ip().default('127.0.0.1'),
     port: z.number().default(6688),
     batchSize: z.number().int().positive().default(100),
-    maxRequestsPerSecond: z.number().int().positive().default(5),
+    maxRequestsPerSecond: z.number().int().positive().default(100),
     corsOrigins: z.string().default('*'),
 })
 
