b4bf14f3cda42a359d1e314cd526e017f40f74e6
Diep Dang <kdt310722@gmail.com>
2024-04-14 02:30:58 +0700

    feat: remove jito from combined sender

--- src/modules/sender/combined.ts
+++ src/modules/sender/combined.ts
@@ -1,6 +1,5 @@
 import { createDeferred } from '@kdt310722/utils/promise'
 import type { VersionedTransaction } from '@solana/web3.js'
-import type { Jito } from './jito'
 import type { LiteRpc } from './lite-rpc'
 import type { Rpc } from './rpc'
 import { type BuildTransactionParams, Sender } from './sender'
@@ -8,7 +7,7 @@ import { type BuildTransactionParams, Sender } from './sender'
 export class Combined extends Sender {
     public override readonly features = { name: 'Combined', tip: false, antiMev: false }
 
-    public constructor(protected readonly jito: Jito, protected readonly rpc: Rpc, protected readonly liteRpc: LiteRpc) {
+    public constructor(protected readonly rpc: Rpc, protected readonly liteRpc: LiteRpc) {
         super()
     }
 
@@ -20,12 +19,11 @@ export class Combined extends Sender {
         const signature = createDeferred<string>()
 
         const rpc = this.rpc.sendTransaction(transaction).then((i) => signature.resolve(i)).catch((error) => error)
-        const jito = this.jito.sendTransaction(transaction).then((i) => signature.resolve(i)).catch((error) => error)
         const liteRpc = this.liteRpc.sendTransaction(transaction).then((i) => signature.resolve(i)).catch((error) => error)
 
-        Promise.allSettled([rpc, jito, liteRpc]).then(async () => {
+        Promise.allSettled([rpc, liteRpc]).then(async () => {
             if (!signature.isSettled) {
-                signature.reject(new AggregateError(await Promise.all([rpc, jito, liteRpc]), 'All senders failed'))
+                signature.reject(new AggregateError(await Promise.all([rpc, liteRpc]), 'All senders failed'))
             }
         })
 
