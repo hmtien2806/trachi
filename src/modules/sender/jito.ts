dd6f1d98370499151a27f3dd9fba3f99e79c28a4
Diep Dang <kdt310722@gmail.com>
2024-04-14 02:17:24 +0700

    refactor: change log level

--- src/modules/sender/jito.ts
+++ src/modules/sender/jito.ts
@@ -52,7 +52,7 @@ export class Jito extends Sender {
                         })
                     },
                     (error) => {
-                        this.logger.error(`Unable to send transaction to ${highlight(connection.rpcEndpoint)}`, error)
+                        this.logger.warn(`Unable to send transaction to ${highlight(connection.rpcEndpoint)}`, error)
                     },
                 )
 
@@ -75,7 +75,7 @@ export class Jito extends Sender {
         }
 
         return tap(await execute(), (signature) => {
-            const stop = poll(async () => execute(true).catch((error) => tap(this.logger.error(error), () => this.emit('confirm', signature))), 500)
+            const stop = poll(async () => execute(true).catch((error) => tap(this.logger.warn(error), () => this.emit('confirm', signature))), 500)
             const timer = setTimeout(() => this.emit('confirm', signature), 5 * 1000)
 
             const onConfirm = (tx: string) => {
