7f2d24f19058725eb76cf661e63ee0c6bf79f2e8
Diep Dang <kdt310722@gmail.com>
2024-04-18 09:41:03 +0700

    feat: disable graceful shutdown

--- src/core/logger.ts
+++ src/core/logger.ts
@@ -1,7 +1,7 @@
 import { type ChildLoggerOptions, createDefaultLogger } from '@kdt310722/logger'
 import { config } from '../config/config'
 
-export const logger = createDefaultLogger(config.logger)
+export const logger = createDefaultLogger({ ...config.logger, handleExit: false })
 
 export function createChildLogger(name?: string, options?: ChildLoggerOptions) {
     return logger.child({ name, ...options })
