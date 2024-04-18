0cde6f0622e0b1c58e11172a98cba8033d6e7d5e
Diep Dang <kdt310722@gmail.com>
2024-04-18 22:42:18 +0700

    feat: cache requests

--- src/rpc-methods/raydium/find-pool-keys-by-pair.ts
+++ src/rpc-methods/raydium/find-pool-keys-by-pair.ts
@@ -1,8 +1,11 @@
 import { isArray } from '@kdt310722/utils/array'
 import { notNullish } from '@kdt310722/utils/common'
+import type { PublicKey } from '@solana/web3.js'
 import { z } from 'zod'
 import type { RpcMethod } from '../../common/rpc'
 import { logger } from '../../core/logger'
+import type { Context } from '../../types/context'
+import { Cache } from '../../utils/cache'
 import { formatRaydiumAmmV4PoolKeys } from '../../utils/formatters/format-raydium-amm-v4-pool-keys'
 import { toJson } from '../../utils/json'
 import { publicKey } from '../../utils/rules/public-key'
@@ -10,9 +13,7 @@ import { publicKey } from '../../utils/rules/public-key'
 export function createFindPoolKeysByPairHandler(): RpcMethod {
     const schema = z.union([z.tuple([publicKey, publicKey]), z.object({ tokenA: publicKey, tokenB: publicKey })]).transform((value) => (isArray(value) ? value : [value.tokenA, value.tokenB]))
 
-    return async (params, { raydiumAmmV4Pool, market, raydiumAmmV4Liquidity }) => {
-        const [tokenA, tokenB] = schema.parse(params)
-
+    const find = async (tokenA: PublicKey, tokenB: PublicKey, { raydiumAmmV4Pool, market, raydiumAmmV4Liquidity }: Context) => {
         const pools = await raydiumAmmV4Pool.findByPair(tokenA, tokenB, true).then((pools) => Promise.all(pools.map(async (pool) => {
             try {
                 const [poolMarket, reserves] = await Promise.all([
@@ -29,5 +30,19 @@ export function createFindPoolKeysByPairHandler(): RpcMethod {
         })))
 
         return pools.filter(notNullish).sort((a, b) => b.liquidity.sub(a.liquidity).toNumber()).map((i) => toJson(i))
+    }
+
+    const requests = new Cache<ReturnType<typeof find>>()
+
+    return async (params, context) => {
+        const [tokenA, tokenB] = schema.parse(params)
+        const key = `${tokenA.toString()}-${tokenB.toString()}`
+        const request = requests.get(key)
+
+        if (request) {
+            return request
+        }
+
+        return requests.setWithExpire(key, find(tokenA, tokenB, context), 5 * 1000)
     }
 }
