cd442f48444eee4484884f18cfa1c090d5c328c8
Diep Dang <kdt310722@gmail.com>
2024-05-25 04:55:41 +0700

    feat: update

--- src/modules/sender/rpc.ts
+++ src/modules/sender/rpc.ts
@@ -1,5 +1,5 @@
 import { tap } from '@kdt310722/utils/function'
-import { poll, withRetry, withTimeout } from '@kdt310722/utils/promise'
+import { withRetry, withTimeout } from '@kdt310722/utils/promise'
 import { TxVersion } from '@raydium-io/raydium-sdk'
 import type { Connection, VersionedTransaction } from '@solana/web3.js'
 import { connection } from '../../common/connection'
@@ -29,13 +29,8 @@ export class Rpc extends Sender {
         const send = async () => await withRetry(async () => withTimeout(this.connection.sendTransaction(transaction, { skipPreflight: true, maxRetries: 0, preflightCommitment: this.connection.commitment }), 1000, `Timeout when send transaction to RPC`), this.retries)
 
         return tap(await send(), (signature) => {
-            const stop = poll(async () => send().catch((error) => this.logger.error(error)), this.pollingInterval)
-            const timer = setTimeout(() => this.emit('confirm', signature), 5 * 1000)
-
             const onConfirm = (tx: string) => {
                 if (tx === signature) {
-                    clearTimeout(timer)
-                    stop()
                     this.off('confirm', onConfirm)
                 }
             }
